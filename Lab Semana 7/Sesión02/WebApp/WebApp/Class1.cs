7da4219d9c32c9e9c7b7485e87d40b9c928a2f40
Josef Romero Florida <josef.romeroflorida@gmail.com>
2023-05-04 23:15:11 -0500

    Laboratorio-Semana7

---
+++ Lab Semana 7/Sesión02/WebApp/WebApp/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace WebApp
+{
+    public static class TaxConstants
+    {
+        public const double TaxRate = 0.18;
+    }
+}
---
+++ Lab Semana 7/Sesión02/WebApp/WebApp/Sale.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using WebApp;
+namespace WebApp
+{
+	public class Sale
+	{
+		public Sale()
+		{
+		}
+
+		public int GetPriceDiscount(int originalPrice, int discount)
+		{
+			return originalPrice - originalPrice * discount / 100 ;
+		}
+
+        public string greet(string name)
+        {
+			return "Hello," + name;
+        }
+
+        public int getMax(int value1)
+        {
+            return value1;
+        }
+
+
+		public double CalculateDiscountedPrice(double originalPrice, double discount, bool hasDiscount)
+        {
+            if (hasDiscount)
+            {
+                double discountAmount = originalPrice * discount / 100;
+                return originalPrice - discountAmount;
+            }
+            else
+            {
+                return originalPrice;
+            }
+        }
+
+		public double CalculateFinalPrice(double taxprice, bool hasTax)
+        {
+            if (hasTax)
+            {
+                return taxprice + taxprice * TaxConstants.TaxRate;
+            }
+            else
+            {
+                return taxprice;
+            }
+        }
+
+        public double GetFinalPrice(double originalPrice, double discount, bool hasDiscount, bool hasTax)
+        {
+            double discountedPrice = CalculateDiscountedPrice(originalPrice, discount, hasDiscount);
+            return CalculateFinalPrice(discountedPrice, hasTax);
+        }
+
+    }
+}
+
