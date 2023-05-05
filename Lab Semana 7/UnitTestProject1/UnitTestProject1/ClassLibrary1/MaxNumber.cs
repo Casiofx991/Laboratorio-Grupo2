894dc71af1f5975e6ca12719e3f2c629b6627865
Josef Romero Florida <josef.romeroflorida@gmail.com>
2023-05-04 23:11:43 -0500

    LaboratorioSemana7

---
+++ Lab Semana 7/UnitTestProject1/UnitTestProject1/ClassLibrary1/MaxNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class MaxNumber
+    {
+       
+            public MaxNumber()         {         }
+      
+        public  int ObtenerMayor(int num1, int num2)
+        {
+            if (num1 > num2)
+            {
+                return num1;
+            }
+            else
+            {
+                return num2;
+            }
+        }
+    }
+}
---
+++ Lab Semana 7/UnitTestProject1/UnitTestProject1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,42 @@
+﻿using ClassLibrary1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Xunit;
+
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Max1()
+        {
+            MaxNumber maxnumber = new MaxNumber();
+            int result = maxnumber.ObtenerMayor(100, 20);
+
+
+            Assert.Equals(80, result);
+        }
+        [Fact]
+        public void Max2()
+        {
+            MaxNumber maxnumber = new MaxNumber();
+            int result = maxnumber.ObtenerMayor(30, 15);
+
+
+            Assert.Equals(50, result);
+        }
+        [Fact]
+        public void Max3()
+        {
+            MaxNumber maxnumber = new MaxNumber();
+            int result = maxnumber.ObtenerMayor(50, 10);
+
+
+            Assert.Equals(10, result);
+        }
+
+    }
+}
+
